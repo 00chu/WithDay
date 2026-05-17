448f69e20eaaf1827737247b2867363eb65f3d88
yglee <yglee0248@gmail.com>
2026-05-18 00:32:27 +0900

    vite.config.js 수정

--- WithDayFront/vite.config.js
+++ WithDayFront/vite.config.js
@@ -6,6 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    // 구글 로그인 팝업 에러(COOP) 해결을 위한 보안 헤더 출입증 추가
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups", 
+    }
   }
 })
