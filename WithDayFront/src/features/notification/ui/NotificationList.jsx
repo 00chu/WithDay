3a4250eb9b97fda10b9d52896f7534457b56fbbe
00chu <gamjachoi00@naver.com>
2026-05-28 10:52:53 +0900

    전체 삭제, 읽은 알림 삭제 추가

--- WithDayFront/src/features/notification/ui/NotificationList.jsx
+++ WithDayFront/src/features/notification/ui/NotificationList.jsx
@@ -1,6 +1,12 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
-import { getNotifications, readNotification, deleteNotification } from "../api";
+import {
+  getNotifications,
+  readNotification,
+  deleteNotification,
+  deleteReadNotifications,
+  deleteAllNotifications,
+} from "../api";
 import styles from "./Notification.module.css";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 
@@ -79,19 +85,21 @@ export default function NotificationList({ onClose }) {
   const handleDeleteRead = async () => {
     await deleteReadNotifications();
 
+    // 목록 다시 조회
     await queryClient.invalidateQueries({
       queryKey: ["notifications"],
     });
 
+    // 빨간 점 갱신
     await queryClient.invalidateQueries({
       queryKey: ["notification-count"],
     });
   };
 
-  const handleDeleteNotification = async (e, notificationId) => {
+  const handleDeleteNotification = async () => {
     e.stopPropagation();
 
-    await deleteNotification(notificationId);
+    await deleteAllNotifications();
 
     // 목록 다시 조회
     await queryClient.invalidateQueries({
