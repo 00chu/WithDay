ca5ecd3cb10baf8ab240bd0bde0bd8a3d02b817f
xxiyeon <tiaqhfl@nate.com>
2026-06-10 10:49:19 +0900

    수정

--- WithDayFront/src/page/home/HomeCarousel.jsx
+++ WithDayFront/src/page/home/HomeCarousel.jsx
@@ -13,42 +13,37 @@ const CAROUSEL_ITEMS = [
   },
   {
     id: "withday-museum",
-    image: "/artGallery",
+    image: "/artGallery.png",
     title: "전시회는 좋은데, 감상평 혼잣말은 좀 그렇잖아요",
     description: "취향 맞는 사람과 전시·문화 일정을 함께 즐겨보세요.",
   },
   {
     id: "withday-travel",
-    image:
-      "/scenery",
+    image: "/scenery.png",
     title: "이번 주말도 집콕? 그건 날씨한테 예의가 아니죠",
     description: "근교 여행, 당일치기, 가벼운 나들이 일정을 만나보세요.",
   },
   {
     id: "withday-dining",
-    image:
-      "/izakaya",
+    image: "/izakaya.png",
     title: "맛집은 찾았는데 같이 갈 사람이 없다면",
     description: "퇴근 후 한 끼, 카페, 술집 번개까지 부담 없이 시작해보세요.",
   },
   {
     id: "withday-activity",
-    image:
-      "/trip",
+    image: "/trip.png",
     title: "운동은 싫지만 액티비티는 또 못 참지",
     description: "등산, 러닝, 서핑, 클라이밍처럼 움직이는 일정을 둘러보세요.",
   },
   {
     id: "withday-culture",
-    image:
-      "/concert",
+    image: "/concert.png",
     title: "공연 끝나고 ‘와 좋았다’만 하기 아쉽다면",
     description: "같은 취향의 위트와 공연·축제·전시 이야기를 나눠보세요.",
   },
   {
     id: "withday-gathering",
-    image:
-      "/busStop",
+    image: "/busStop.png",
     title: "새로운 사람? 부담 없이, 어색함은 짧게",
     description: "동네 산책, 카페 대화, 소규모 모임을 가볍게 시작해보세요.",
   },
